f72eaf3f77c10c0a31127b2ec9621f11c6dfc107
reggiegg <reggie.gillett@gmail.com>
2014-10-15 12:53:24 -0700

    lazied! (maybe)! Stricts all over, and not in if (possible over strict)

--- xinterp-lazy.rkt
+++ xinterp-lazy.rkt
@@ -294,14 +294,22 @@
 (define (run sexp)
   (interp (pre-process (parse sexp))))
 
+
+
 ;; interp : CFWAE -> CFWAE-Value
 ;; This procedure interprets the given CFWAE and produces a result 
 ;; in the form of a CFWAE-Value (either a closureV, thunkV, or numV).
 ;; (Assumes the input was successfully produced by pre-process.)
 (define (interp expr)
-  (local [(define (helper expr env)
+  (local [(define (strict expr)
+            (type-case CFWAE-Value expr
+              [thunkV (expr env)
+                      (strict (helper expr env))]
+              [else expr]))
+          (define (helper expr env)
             (type-case CFWAE expr
               [num (n) (numV n)]
+              [id (id) (lookup id env)]
               [binop (op lhs rhs)
                      (local [(define helped-lhs (helper lhs env))
                              (define helped-rhs (helper rhs env))]
@@ -312,7 +320,7 @@
               [if0 (cond-expr then-expr else-expr)
                    (local [(define cond-val (helper cond-expr env))]
                      (if (numV? cond-val)
-                         (if (= 0 (numV-n (helper cond-expr env)))
+                         (if (= 0 (numV-n (strict (helper cond-expr env))))
                              (helper then-expr env)
                              (helper else-expr env))
                          (error "non-numeric condition value")))]
@@ -321,7 +329,7 @@
                        (thunkV body-expr env)
                        (closureV (first args) body-expr env))]
               [app (fun-expr arg-exprs)
-                   (local [(define fun-val (helper fun-expr env))]
+                   (local [(define fun-val (strict (helper fun-expr env)))]
                      (type-case CFWAE-Value fun-val
                        [thunkV (body thunk-env) (if (empty? arg-exprs)
                                                     (helper body thunk-env)
@@ -331,11 +339,11 @@
                                      (error "too few arguments")
                                      (helper body
                                              (anEnv param
-                                                    (helper (first arg-exprs) env)
+                                                    (thunkV (first arg-exprs) env)
                                                     closure-env)))]
                        [numV (n) (error 'interp "invalid function expression: ~s" n)]))]
-              [else (error "interpretor error")]))]
-    (helper expr (mtEnv))))
+              [else (error 'interp "interpretor error: ~s" expr )]))]
+    (strict (helper expr (mtEnv)))))
 
 ;(numV-n (interp (pre-process
 ;                 (with (binding (quote apply)
@@ -379,7 +387,7 @@
 (test (interp (if0 (binop + (num 1) (num 2)) (num 1) (num 2))) (numV 2))
 
 (test (interp (fun (list 'x) (id 'x))) (closureV 'x (id 'x) (mtEnv)))
-(test (interp (fun empty (id 'x))) (thunkV (id 'x) (mtEnv)))
+
 
 
 
